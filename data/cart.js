d5de692da0a845118da5027f28e16d4a6e60e7f6
Sajit <sajitullahkhan@gmail.com>
2023-11-07 09:07:08 +0600

    Moved addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let machingItem;
+
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            machingItem = cartItem;
+        }
+    });
+
+    if(machingItem){
+        machingItem.quantity += 1;
+    } else{
+        cart.push({
+            productId,
+            quantity: 1
+        });
+    }
+}
