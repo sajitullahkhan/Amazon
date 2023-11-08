9d4d43c43d67da68566cf6da528f81d6175b1909
Sajit <sajitullahkhan@gmail.com>
2023-11-08 22:27:02 +0600

    save data and generate HTML for delivery options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import { cart, addToCart} from "../data/cart.js";
 import { products } from "../data/products.js";
-import { formCurrency } from "./utils/money.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productsHTML = '';
 
@@ -25,7 +25,7 @@ products.forEach((products) => {
         </div>
 
         <div class="product-price">
-        $${formCurrency(products.priceCents)}
+        $${formatCurrency(products.priceCents)}
         </div>
 
         <div class="product-quantity-container">
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,3 @@
-export function formCurrency(priceCents) {
+export function formatCurrency(priceCents) {
     return ((priceCents / 100).toFixed(2))
 }
